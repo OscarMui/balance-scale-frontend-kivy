01046f17bb04051d9d408709316ccd9abeee6af0
Oscar Mui <oscarmui1127@gmail.com>
2024-08-11 22:24:28 +0800

    client version 20240811.0.app

--- common/constants.py
+++ common/constants.py
@@ -7,7 +7,7 @@ SSL = PRODUCTION
 SERVER_URL = f'http{"s" if SSL else ""}://{SERVER_IP}'
 WSS_URL = f'ws{"s" if SSL else ""}://{SERVER_IP}/game'
 
-CLIENT_VERSION = "20240106.0.app"
+CLIENT_VERSION = "20240811.0.app"
 
 DISCORD_URL = "https://discord.gg/H6bsxqZyuu"
 
